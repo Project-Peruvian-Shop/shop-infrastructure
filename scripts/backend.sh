618c1b3f1c7e6f8fa1f3d335a87e138a62a66f36
tineojf <92743762+tineodev@users.noreply.github.com>
2025-11-11 14:18:22 -0500

    Refactor backend scripts to remove frontend dependency and enhance environment variable sourcing for backend startup

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -4,20 +4,19 @@
 
 set -e
 
-if [ "$#" -ne 3 ]; then
-  echo "Uso: $0 <IP_FRONTEND> <IP_BACKEND> <KEY_SSH>"
+if [ "$#" -ne 2 ]; then
+  echo "Uso: $0 <IP_BACKEND> <KEY_SSH>"
   exit 1
 fi
 
-IP_FRONTEND=$1
-IP_BACKEND=$2
-KEY_SSH=$3
+IP_BACKEND=$1
+KEY_SSH=$2
 USUARIO=ubuntu
 
 # Rutas locales
 REPO_BACKEND_LOCAL="./backend"
-REPO_BACKEND_URL="git@github.com:Syntax-NSS/Hamar-Backend.git"
-BRANCH="test"
+REPO_BACKEND_URL="git@github.com:Project-Peruvian-Shop/shop-backend.git"
+BRANCH="main"
 
 # --- BACKEND ---
 echo "📥 Eliminar carpeta local del backend..."
@@ -26,19 +25,6 @@ rm -rf "$REPO_BACKEND_LOCAL"
 echo "🆕 Clonando repo en la rama $BRANCH..."
 git clone -b $BRANCH "$REPO_BACKEND_URL" "$REPO_BACKEND_LOCAL"
 cd "$REPO_BACKEND_LOCAL" || exit 1
-
-# --- Reemplazar URL del frontend en SecurityConfig.java ---
-echo "🔧 Reemplazando URL del frontend por http://$IP_FRONTEND..."
-SECURITY_CONFIG="src/main/java/eu/newstagesolutions/hammar_api/config/SecurityConfig.java"
-if [ -f "$SECURITY_CONFIG" ]; then
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i "" "s|http://localhost:4200|http://$IP_FRONTEND|g" "$SECURITY_CONFIG"
-  else
-    sed -i "s|http://localhost:4200|http://$IP_FRONTEND|g" "$SECURITY_CONFIG"
-  fi
-else
-  echo "⚠️ No se encontró SecurityConfig.java, continuando..."
-fi
 
 # --- Activar environment local (si existe) ---
 if [ -f "../environment" ]; then
@@ -61,7 +47,6 @@ echo "⚡ Levantando backend en la instancia..."
 ssh -o StrictHostKeyChecking=no -i "$KEY_SSH" "$USUARIO@$IP_BACKEND" "
   sudo chown -R $USUARIO:$USUARIO /home/ubuntu/backend
   cd /home/ubuntu/backend
-  # nohup java -jar *.jar > backend.log 2>&1 &
   exit
 "
 
--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -18,6 +18,13 @@ echo "⚡ Levantando backend en la instancia $IP_BACKEND..."
 ssh -o StrictHostKeyChecking=no -i "$KEY_SSH" "$USUARIO@$IP_BACKEND" "
   sudo chown -R $USUARIO:$USUARIO /home/ubuntu/backend
   cd /home/ubuntu/backend
-  nohup java -jar *.jar > backend.log 2>&1 &
+  source /etc/environment
+  nohup java -jar *.jar \
+    --spring.datasource.url=\$SPRING_DATASOURCE_URL \
+    --spring.datasource.username=\$SPRING_DATASOURCE_USERNAME \
+    --spring.datasource.password=\$SPRING_DATASOURCE_PASSWORD \
+    --spring.profiles.active=\$SPRING_PROFILES_ACTIVE \
+    > backend.log 2>&1 &
   echo '✅ Backend iniciado en background. Log en backend.log'
 "
+
